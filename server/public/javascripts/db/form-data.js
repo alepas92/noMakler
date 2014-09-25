dc92f3f5c82f70250251e2e80175259c822d013d
alepas92 <oleksiy.pastukhov@gmail.com>
2014-09-25 18:12:56 +0300

    Commit

--- server/public/javascripts/db/form-data.js
+++ server/public/javascripts/db/form-data.js
@@ -1,20 +1,50 @@
+//Move next section into another file
+
 $("#add-site").on("click", function() {
 	var siteURL = $("#site-url").val();
 	var phoneTag = $("#phone-tag").val();
 	var advertTag = $("#advert-tag").val();
-	console.log(siteURL, phoneTag, advertTag);
 	document.getElementById("sites-info").innerHTML += "<span class='s-siteURL'>" + siteURL + "</span> "
 								+ "<span class='s-phoneTag'>" + phoneTag + "</span> " 
 								+ "<span class='s-advertTag'>"+ advertTag + "</span><br>";
 	$("#site-url").val('');
 });
 
+$("#saveData").on("click", createFile);
+
+//This section form site objects
+
+function InfAbSite(siteUrl, phoneTag, advertTag) {
+	this.siteUrl = siteUrl,
+	this.phoneTag = phoneTag,
+	this.advertTag = advertTag
+};
+
+//This section form city object with all site info
+
+function FormCityObject(city, cityDistricts, phoneClass, allSiteInfo) {
+	this.cityName = city,
+	this.cityDistricts = cityDistricts,
+	this.phoneClass = phoneClass,
+	this.allSiteInfo = allSiteInfo
+}
+
+//Next function help to form object for current country
+
+function FormObject(country, city, cityDistricts, phoneClass, allSiteInfo) {
+	this.country = country,
+	this.city = new FormCityObject(city, cityDistricts, phoneClass, allSiteInfo)
+}
+
+//Next function is the main function of the forming globalData. Here
+//we get all data from forms and form globalData object
+
 function getGlobalData() {
 	var country = $("#country-selector option:selected").text();
-	var city = $("#city").text();
-	var cityDistricts = $("city-district").text();
+	var city = $("#city").val();
+	var cityDistricts = $("#city-district").val();
 		cityDistricts = cityDistricts.split(", ");
-	var phoneClass = $("#phone-class").text();
+	var phoneClass = $("#phone-class").val();
 	var allSiteUrls = $("div").find(".s-siteURL");
 	var allSitePhoneTags = $("div").find(".s-phoneTag");
 	var allSiteAdvertTags = $("div").find(".s-advertTag");
@@ -24,13 +54,17 @@ function getGlobalData() {
 		var phTag = $(allSitePhoneTags[i]).text();
 		var advTag = $(allSiteAdvertTags[i]).text();
 		allSiteInfo[i] = new InfAbSite(url, phTag, advTag);
-		console.log(allSiteInfo[i]);
 	}
-
+	var globalData = new FormObject(country, city, cityDistricts, phoneClass, allSiteInfo);
+	return globalData;
 }
 
-function InfAbSite(siteUrl, phoneTag, advertTag) {
-	this.siteUrl = siteUrl,
-	this.phoneTag = phoneTag,
-	this.advertTag = advertTag
-};
+
+function createFile() {
+	var data = getGlobalData();
+	var file = JSON.stringify(data);
+	var filemane = 'db_' + data.country + '_' + data.city.cityName;
+	var blob = new Blob([file], {type: "application/json"});
+	saveAs(blob, filemane + ".json");   
+}
+
